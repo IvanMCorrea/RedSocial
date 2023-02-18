706cad0e516b4a59856c15a9944e15342afed9e6
Ivan Correa <ivaanmc95@gmail.com>
2023-02-18 12:48:25 -0300

    Arreglo funcion para carga de usuarios de rick & morty

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -284,7 +284,10 @@ const Register = () => {
                       Register
                     </button>
                     <button
-                      onClick={getAllCharacters}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        getAllCharacters();
+                      }}
                       className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                       getAllCharacters
--- src/services/getCharacters.ts
+++ src/services/getCharacters.ts
@@ -1,8 +1,7 @@
-import { exportDataToFirestore } from "../services/db";
 import defaultImage from "../assets/default_avatar.png";
 import { createNewUser } from "../api/auth";
 
-export const getCharacters = () => {
+/* export const getCharacters = () => {
   let charUrl = "https://rickandmortyapi.com/api/character";
   let charList: Array<object> = [];
   const getUsers = async () => {
@@ -19,23 +18,24 @@ export const getCharacters = () => {
     }
   };
   getUsers();
-};
+}; */
 export const getAllCharacters = async () => {
   for (let index = 1; index < 42; index++) {
     let charUrl = `https://rickandmortyapi.com/api/character?page=${index}`;
-    let accountList: Array<FormData> = [];
     try {
-      const formData = new FormData();
       let datos = await fetch(charUrl);
       let response = await datos.json();
       let avatar = null;
       await response.results.forEach((element: any) => {
+        const formData = new FormData();
         let name = element.name.replace(/ /g, "");
         let pass = name;
+        let email = `${name}Account@mail.com`;
         avatar = element.image ? element.image : null;
         let account = {
           username: name,
           password: pass,
+          email: email,
           ...element,
         };
         for (const key in account) {
@@ -53,38 +53,11 @@ export const getAllCharacters = async () => {
               formData.append("avatar", file);
             })
             .then(() => {
-              accountList = [...accountList, formData];
+              createNewUser(formData);
             });
       });
-      accountList &&
-        accountList[0] &&
-        accountList.map((newAccount: any) => {
-          createNewUser(newAccount);
-        });
-      /* exportDataToFirestore(accountList); */
     } catch (error) {
       console.error(error);
     }
   }
-};
-
-export const getAccounts = () => {
-  let charUrl = "https://rickandmortyapi.com/api/character";
-  let accountList: Array<object> = [];
-  const getUsers = async () => {
-    try {
-      let datos = await fetch(charUrl);
-      let response = await datos.json();
-      response.results.forEach((element: any) => {
-        let name = element.name.replace(/ /g, "");
-        let pass = "prueba123";
-        let account = { username: name, password: pass, ...element };
-        accountList = [...accountList, account];
-      });
-      exportDataToFirestore(accountList);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  getUsers();
 };
