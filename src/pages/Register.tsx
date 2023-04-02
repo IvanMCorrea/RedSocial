19d4ea76a6beb2bf2cd547a1b0fefcb7154dcdb7
Ivan Correa <ivaanmc95@gmail.com>
2023-04-02 19:26:16 -0300

    Comento btn para crear usuarios

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -285,7 +285,7 @@ const Register = () => {
                     >
                       Register
                     </button>
-                    <button
+                    {/* <button
                       onClick={(e) => {
                         e.preventDefault();
                         getAllCharacters();
@@ -293,7 +293,7 @@ const Register = () => {
                       className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                       getAllCharacters
-                    </button>
+                    </button> */}
                   </div>
                 </div>
               </form>
